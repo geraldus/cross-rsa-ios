5d9a282193ef7c676af33968368711113c2dbb12
Arthur Sakhievich Fayzrakhmanov <heraldhoi@gmail.com>
2021-10-09 01:45:03 +0500

    Add main app file

---
+++ pgpt1/pgpt1App.swift
@@ -0,0 +1,17 @@
+//
+//  pgpt1App.swift
+//  pgpt1
+//
+//  Created by Arthur Fayzrakhmanov on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct pgpt1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
